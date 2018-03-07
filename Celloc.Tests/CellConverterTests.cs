ae7cdbbd17149b6c256d9f84477b44565ecde99e
Sydney Du Plooy <DuPlooySy@aforbes.co.za>
2018-03-07 19:55:25 +0200

    Initial commit.

---
+++ Celloc.Tests/CellConverterTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Celloc.Tests
+{
+	[TestFixture]
+    public class When_calling_convert_on_cell_converter
+    {
+		[Test]
+		public void It_should_throw_an_exception_when_the_row_is_out_of_range()
+		{
+			var exception = Assert.Throws<ArgumentOutOfRangeException>(() => CellConverter.Parse("XFE1048577"));
+			Assert.AreEqual("Row must be greater than 0 and less than 1048576.\r\nParameter name: row", exception.Message);
+		}
+
+		[Test]
+		public void It_should_throw_an_exception_when_the_cell_is_not_in_the_correct_format()
+		{
+			var exception = Assert.Throws<Exception>(() => CellConverter.Parse("1A"));
+			Assert.AreEqual("1A is not a valid cell format.", exception.Message);
+		}
+
+		[Test]
+		public void It_should_convert_the_cell_to_an_integer_tuple()
+		{
+			Assert.AreEqual(new Tuple<int, int>(1, 1), CellConverter.Parse("A1"));
+			Assert.AreEqual(new Tuple<int, int>(16384, 1048576), CellConverter.Parse("XFD1048576"));
+		}
+
+	    [Test]
+	    public void It_should_convert_the_cell_to_a_zero_based_integer_tuple()
+	    {
+		    Assert.AreEqual(new Tuple<int, int>(0, 0), CellConverter.Parse("A1", Offset.ZeroBased));
+		    Assert.AreEqual(new Tuple<int, int>(16383, 1048575), CellConverter.Parse("XFD1048576", Offset.ZeroBased));
+	    }
+	}
+}
---
+++ Celloc/CellConverter.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Celloc
+{
+	public enum Offset
+	{
+		None,
+		ZeroBased
+	}
+
+    public static class CellConverter
+    {
+		private const string ColumnRowPattern = "^(?<column>[a-zA-Z]{0,3})(?<row>[0-9]+)$";
+
+		public static Tuple<int, int> Parse(string cell, Offset offset = Offset.None)
+		{
+			if (string.IsNullOrEmpty(cell))
+				throw new ArgumentNullException(nameof(cell));
+
+			var regex = new Regex(ColumnRowPattern);
+
+			ValidateCellFormat(cell, regex);
+
+			var match = regex.Match(cell);
+
+			var column = match.Groups["column"].Value;
+			var row = Convert.ToInt32(match.Groups["row"].Value);
+
+			ValidateRowRange(row);
+
+			var columnNumber = ConvertColumnToNumber(column);
+
+			return ApplyOffset(offset, columnNumber, row);
+		}
+
+	    private static Tuple<int, int> ApplyOffset(Offset offset, int column, int row)
+	    {
+		    return offset == Offset.None ? new Tuple<int, int>(column, row) : new Tuple<int, int>(column - 1, row - 1);
+	    }
+
+	    private static void ValidateRowRange(int row)
+	    {
+		    if (row < 0 || row > 1048576)
+			    throw new ArgumentOutOfRangeException(nameof(row), "Row must be greater than 0 and less than 1048576.");
+	    }
+
+	    private static void ValidateCellFormat(string cell, Regex regex)
+	    {
+		    if (!regex.IsMatch(cell))
+			    throw new Exception($"{cell} is not a valid cell format.");
+	    }
+
+	    private static int ConvertColumnToNumber(string column)
+	    {
+		    if (string.IsNullOrEmpty(column))
+			    throw new ArgumentNullException(nameof(column));
+
+		    column = column.ToUpperInvariant();
+
+		    var sum = 0;
+
+		    foreach (var letter in column)
+		    {
+			    sum *= 26;
+			    sum += (letter - 'A' + 1);
+		    }
+
+		    return sum;
+	    }
+	}
+}
